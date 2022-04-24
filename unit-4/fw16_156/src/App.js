d8f76ad6e5afef8a2c18e1766bcc434104a141ec
Arpit Surana <suranaarpit34@gmail.com>
2022-04-24 11:15:50 -0700

    err fixed

--- unit-4/fw16_156/src/App.js
+++ unit-4/fw16_156/src/App.js
@@ -5,17 +5,17 @@ import "./App.css";
 // import Day2 from "./sprint-1/day-2/Day2";
 // import Day3 from "./sprint-1/day-3/Day3"
 // import Sprint2Day2 from './sprint-2/day-2/Sprint2Day2';
-import Sprint2Day3 from "./sprint-2/day-3/Sprint2Day3";
+// import Sprint2Day3 from "./sprint-2/day-3/Sprint2Day3";
 
 function App() {
   return (
     <>
-      {/* <Day1 ></Day1>
-      <Day1_2 ></Day1_2> */}
+      {/* <Day1 ></Day1>}
+      {<Day1_2 ></Day1_2> */}
       {/* <Day2></Day2> */}
       {/* <Day3></Day3> */}
       {/* <Sprint2Day2></Sprint2Day2> */}
-      <Sprint2Day3></Sprint2Day3>
+      {/* <Sprint2Day3></Sprint2Day3> */}
     </>
   );
 }
