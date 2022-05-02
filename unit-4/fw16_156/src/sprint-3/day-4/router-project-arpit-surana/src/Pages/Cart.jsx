530cd4b846d367c0ce6ac3097c07a9cb6c542fba
Arpit Surana <suranaarpit34@gmail.com>
2022-05-02 12:50:24 +0530

    Cart Page Updated

--- unit-4/fw16_156/src/sprint-3/day-4/router-project-arpit-surana/src/Pages/Cart.jsx
+++ unit-4/fw16_156/src/sprint-3/day-4/router-project-arpit-surana/src/Pages/Cart.jsx
@@ -30,56 +30,71 @@ function Cart() {
   const successMessage = () => {
     notify()
   }
-
-  return (
-    <>
-      <AnnounceBar />
-      <NavBar />
-      <hr />
+  if(!data.length){
+    return(
       <div>
-        <section className="py-4 container">
-          <div className="row justify-center">
-            <div>
-              <div><h5 className="text-indigo-600 text-2xl font-bold text-center">Total Products in Cart: <span className="text-black">({data.length})</span></h5></div>
-              {/* <div className="text-xl font-bold mr-5"><h1>Total Amount: ₹2499</h1></div> */}
+         <AnnounceBar />
+        <NavBar />
+        <hr />
+        <img src="https://cdn.dribbble.com/users/1010436/screenshots/11302442/empty_cart.jpg" alt="Empty Cart Image" className="h-[500px] m-auto" />
+        <hr />
+        <Footer />
+      </div>
+    )
+  }
+  else{
+    return (
+      <>
+        <AnnounceBar />
+        <NavBar />
+        <hr />
+        <div>
+          <section className="py-4 container">
+            <div className="row justify-center">
+              <div>
+                <div><h5 className="text-indigo-600 text-2xl font-bold text-center">Total Products in Cart: <span className="text-black">({data.length})</span></h5></div>
+                {/* <div className="text-xl font-bold mr-5"><h1>Total Amount: ₹2499</h1></div> */}
+              </div>
+              <br />
+              <hr />
+              <br />
+              {data.map((cartItems) => {
+                return (
+                  <div key={cartItems.id}>
+                    <section className=" hover:bg-gray-100 cursor-pointer flex">
+                      <div
+                        className="h-24 mx-12"
+                        style={{ width: "200px", height: "fit-content" }}
+                      >
+                        <img src={cartItems.pImg} />
+                      </div>
+                      <div className="h-24 w-[800px] mt-9">
+                        <h1 className="text-xl py-5 text-center underline">{cartItems.pName}</h1>
+                      </div>
+                      <div className="h-24 mt-9 w-40">
+                      <h3 className="text-xl py-5 text-center">{cartItems.pAmount}</h3>
+                      </div>
+                      <div className="h-24 mt-9 w-40">
+                      <h2 className="text-xl py-5 text-center underline font-bold">Remove</h2>
+                      </div>
+                    </section>
+                  </div>
+                );
+              })}
             </div>
-            <br />
-            <hr />
-            <br />
-            {data.map((cartItems) => {
-              return (
-                <div key={cartItems.id}>
-                  <section className=" hover:bg-gray-100 cursor-pointer flex">
-                    <div
-                      className="h-24 mx-12"
-                      style={{ width: "200px", height: "fit-content" }}
-                    >
-                      <img src={cartItems.pImg} />
-                    </div>
-                    <div className="h-24 w-[800px] mt-9">
-                      <h1 className="text-xl py-5 text-center underline">{cartItems.pName}</h1>
-                    </div>
-                    <div className="h-24 mt-9 w-40">
-                    <h3 className="text-xl py-5 text-center">{cartItems.pAmount}</h3>
-                    </div>
-                    <div className="h-24 mt-9 w-40">
-                    <h2 className="text-xl py-5 text-center underline font-bold">Remove</h2>
-                    </div>
-                  </section>
-                </div>
-              );
-            })}
-          </div>
-        </section>
-      </div>
-      <div>
-        <button onClick={()=>successMessage()} className="btn mx-[700px] my-5 text-white bg-[#8a4af3] rounded-md shadow-md p-3 hover:scale-[1.1] ease-in duration-100 cursor-pointer hover:bg-blue-500">PAY</button>
-      </div>
-      <hr />
-      <Footer />
-      <ToastContainer/>
-    </>
-  );
+          </section>
+        </div>
+        <div>
+          <button onClick={()=>successMessage()} className="btn mx-[700px] my-5 text-white bg-[#8a4af3] rounded-md shadow-md p-3 hover:scale-[1.1] ease-in duration-100 cursor-pointer hover:bg-blue-500">PAY</button>
+        </div>
+        <hr />
+        <Footer />
+        <ToastContainer/>
+      </>
+    );
+  }
+
+  
 }
 
 export default Cart;
