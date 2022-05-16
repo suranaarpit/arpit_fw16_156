4c2b2047f50680f7f44f56a556503e753e130989
Arpit Surana <suranaarpit34@gmail.com>
2022-05-16 16:52:35 +0530

    added folders

---
+++ unit-5/RCT-201/sprint-1/Day1/index.ts
@@ -0,0 +1,30 @@
+type UserInfo = {
+  id: number;
+  name: string;
+  age: number;
+  salary: number;
+};
+
+const user: UserInfo[] = [
+  { id: 1, name: "Arpit", age: 20, salary: 500000 },
+  { id: 2, name: "Ankur", age: 25, salary: 1000000 },
+  { id: 3, name: "Bala", age: 19, salary: 10000 },
+  { id: 4, name: "Chandra", age: 30, salary: 2000000 },
+  { id: 5, name: "Nrupul", age: 40, salary: 5000000 },
+  { id: 6, name: "Akshay", age: 29, salary: 200000 },
+  { id: 7, name: "Ajay", age: 30, salary: 100000 },
+  { id: 8, name: "Deepak", age: 27, salary: 600000 },
+];
+console.table(user);
+
+// using inbuild .sort method
+
+// const sort = (user: UserInfo[]) =>
+//   user.sort((userA: UserInfo, userB: UserInfo) => {
+//     return userA.age - userB.age;
+//   });
+// const sortedAge = sort(user);
+// console.table(sortedAge);
+
+// using own custom sort method: [Bubble Sort]
+
