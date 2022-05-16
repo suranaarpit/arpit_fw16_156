146a539818c72194929d71e8443a013868430e59
Arpit Surana <suranaarpit34@gmail.com>
2022-05-16 17:33:19 +0530

    sprint-1 day-1 assign. done

--- unit-5/RCT-201/sprint-1/Day1/index.ts
+++ unit-5/RCT-201/sprint-1/Day1/index.ts
@@ -8,9 +8,9 @@ type UserInfo = {
 const user: UserInfo[] = [
   { id: 1, name: "Arpit", age: 20, salary: 500000 },
   { id: 2, name: "Ankur", age: 25, salary: 1000000 },
-  { id: 3, name: "Bala", age: 19, salary: 10000 },
-  { id: 4, name: "Chandra", age: 30, salary: 2000000 },
-  { id: 5, name: "Nrupul", age: 40, salary: 5000000 },
+  { id: 3, name: "Shubham", age: 19, salary: 10000 },
+  { id: 4, name: "Ranveer", age: 30, salary: 2000000 },
+  { id: 5, name: "Manan", age: 40, salary: 5000000 },
   { id: 6, name: "Akshay", age: 29, salary: 200000 },
   { id: 7, name: "Ajay", age: 30, salary: 100000 },
   { id: 8, name: "Deepak", age: 27, salary: 600000 },
